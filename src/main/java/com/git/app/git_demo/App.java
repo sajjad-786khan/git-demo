11e2b13cbea264a662cbbf502f8495d3bfcadab2
Sajjad <sajjadupc@gmail.com>
2021-04-20 10:44:00 +0530

    added if condition to check the data

--- src/main/java/com/git/app/git_demo/App.java
+++ src/main/java/com/git/app/git_demo/App.java
@@ -8,6 +8,9 @@ public class App {
 
 	public void addData(String data) {
 		System.out.println("data==" + data);
+		if (data.equals("mike")) {
+			System.out.println("Welcome Mike");
+		}
 	}
 
 	public static void main(String[] args) {
