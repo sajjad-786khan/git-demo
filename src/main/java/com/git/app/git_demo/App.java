013a6291aaaf5b3219b725035c3c1d210b852deb
sajjad-786khan <81549450+sajjad-786khan@users.noreply.github.com>
2021-08-23 22:36:06 +0530

    method added by developer2

--- src/main/java/com/git/app/git_demo/App.java
+++ src/main/java/com/git/app/git_demo/App.java
@@ -11,6 +11,9 @@ public class App {
 			System.out.println("Welcome Mike");
 		}
 	}
+	public void addName(){
+		System.out.println("Welcome Rock in new method()");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
